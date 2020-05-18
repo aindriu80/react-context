d524d0c2dc33a06fd46bda0a00e64fbafaff3e4c
unknown <aindriu80@gmail.com>
2020-05-18 15:48:42 +0100

    Cart Context

--- src/App.js
+++ src/App.js
@@ -28,9 +28,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    // const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
